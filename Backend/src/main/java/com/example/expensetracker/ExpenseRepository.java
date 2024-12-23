b6466b17789569c840d63440208461293218d71f
champa1987 <champa.swamy@gmail.com>
2024-12-23 07:17:56 +0530

    Delete Backend directory

--- Backend/src/main/java/com/example/expensetracker/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.expensetracker;
-
-import com.example.expensetracker.model.Expense;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-    // Additional query methods can be added here if needed
-}
